d1ecacd68c52e91028e74200e0ba9d64528df836
kat ballo <katballo@icloud.com>
2018-03-15 11:01:14 -0400

    ..

--- server/db/db.js
+++ server/db/db.js
@@ -2,10 +2,7 @@ const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
 
 const connectionString = process.env.DATABASE_URL || `postgres://localhost:5432/${pkg.name}`
-// const connectionString= `postgres://localhost:5432/${pkg.name}` || process.env.DATABASE_URL
-
 const db = new Sequelize(connectionString, {logging: false})
-
 
 // const db = new Sequelize(
 //   process.env.DATABASE_URL || 'postgres://localhost:5432/iloveplant', {
