6125f9bf928d08efed6674cb6578b3b1c44a025e
kat ballo <katballo@icloud.com>
2018-03-15 02:12:52 -0400

    ...

--- client/index.js
+++ client/index.js
@@ -6,8 +6,6 @@ import { UserInterface } from './components'
 import { Router } from 'react-router-dom';
 import history from './history'
 
-// establishes socket connection
-import './socket'
 
 ReactDOM.render(
   <Provider store={store}>
